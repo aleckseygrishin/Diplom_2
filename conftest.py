059695454913081ac8d23bb2c8f4d5a7e8333e7e
aleckseygrishin <combine696@yandex.ru>
2024-06-05 20:41:47 +0300

    added allure result

--- conftest.py
+++ conftest.py
@@ -5,8 +5,8 @@ from burger_api import BurgerApi
 from helper import Helper
 
 
-@pytest.fixture(scope='function')
 @allure.step('Вызываем фикстуру (создание и удаление по окончанию теста пользователя)')
+@pytest.fixture(scope='function')
 def create_and_delete_user(user_data_registration):
     create = BurgerApi.create_user(user_data_registration)
     token = Helper.get_access_token(create)
@@ -16,15 +16,15 @@ def create_and_delete_user(user_data_registration):
     BurgerApi.delete_user(token)
 
 
-@pytest.fixture(scope='function')
 @allure.step('Вызываем фикстуру (создание json для регистрации пользователя)')
+@pytest.fixture(scope='function')
 def user_data_registration():
     return Helper.random_user()
 
 
 # В документации отсутствует удаление заказа, поэтому return, без удаления
-@pytest.fixture(scope='function')
 @allure.step('Вызываем фикстуру (создание заказа, без авторизации)')
+@pytest.fixture(scope='function')
 def create_order():
     get_ingredients_response = BurgerApi.get_ingredients()
     order_body = Helper.get_ingredients_list(get_ingredients_response)
@@ -33,8 +33,8 @@ def create_order():
     return create_order
 
 
-@pytest.fixture(scope='function')
 @allure.step('Вызываем фикстуру (создание заказа, с авторизацией)')
+@pytest.fixture(scope='function')
 def create_order_with_auth(create_and_delete_user):
     token = Helper.get_access_token(create_and_delete_user)
     get_ingredients = BurgerApi.get_ingredients()
